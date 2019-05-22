b590f90f41fdc37b782f29a22165e91456ec7d0b
Bart-Jan Vrielink <bartjan@vrielink.net>
2019-05-22 11:17:42 +0200

    Updates to make the various rake tasks work
    - Added a Gemfile, so bundle exec now works.
    - Added .fixtures.yml, spec/default_facts.yml and updated spec/spec_helper.rb to fix 'rake spec'.
    - Added .rubocop.yml from the PDK template.
    - Updated .gitignore.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,46 @@
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
+
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
 
-require 'mocha'
+require 'puppetlabs_spec_helper/module_spec_helper'
 require 'puppet'
 require 'rspec'
-require 'spec/autorun'
+require 'rspec-puppet-facts'
 
-Spec::Runner.configure do |config|
-    config.mock_with :mocha
+include RspecPuppetFacts
+
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
+
+default_fact_files = [
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml')),
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml')),
+]
+
+default_fact_files.each do |f|
+  next unless File.exist?(f) && File.readable?(f) && File.size?(f)
+
+  begin
+    default_facts.merge!(YAML.safe_load(File.read(f), [], [], true))
+  rescue => e
+    RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
+  end
 end
 
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-    alias :must :should
+RSpec.configure do |c|
+  c.default_facts = default_facts
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+  end
+  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!(95)
+  end
 end
